25804cb3bd2a2aa0eb8bb5ab0369f283ac940dcf
jonathanserb <jonathanserb@gmail.com>
2025-10-16 17:36:20 -0400

    Created a new hardwareConfig for the shooter which extends MotorConfig and allows constants for two motors to be set.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwareConfig/actuators/shooter/ShooterConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwareConfig/actuators/shooter/ShooterConstants.java
@@ -6,25 +6,32 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
-import org.firstinspires.ftc.teamcode.hardwareConfig.baseConstants.MotorConstants;
+import org.firstinspires.ftc.teamcode.hardwareConfig.baseConstants.ShooterConstantsBase;
 
 public class ShooterConstants {
         static {
-                MotorConstants.name = "rightRear";
-                MotorConstants.motorConfigurationType = "clone";
-                MotorConstants.ticksPerRev= 1993.6; // gobilda ticks 84 rpm// 537.7; // gobilda ticks per rev for 312 rpm //1397.1; // gobuilda ticks per rev for 60 rpm;
-                MotorConstants.achievableMaxRPMFraction =1.0;
-                MotorConstants.gearing=1;
-                MotorConstants.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER;
-                MotorConstants.resetMode = DcMotor.RunMode.STOP_AND_RESET_ENCODER;
-                MotorConstants.direction = DcMotorSimple.Direction.REVERSE;
-                MotorConstants.startPosition =0;
-                MotorConstants.targetPosition =0;
-                MotorConstants.feedforward=0.0;
-                MotorConstants.tolerableError=0.7; //in degrees
-                MotorConstants.kp=0.3;  //to be tuned for the PID controller
-                MotorConstants.ki=0;
-                MotorConstants.kd=0;
+                ShooterConstantsBase.frontMotorName = "frontShooterMotor";
+                ShooterConstantsBase.rearMotorName = "frontShooterMotor";
+                ShooterConstantsBase.motorConfigurationType = "clone";
+                ShooterConstantsBase.ticksPerRev= 1993.6; // gobilda ticks 84 rpm// 537.7; // gobilda ticks per rev for 312 rpm //1397.1; // gobuilda ticks per rev for 60 rpm;
+                ShooterConstantsBase.achievableMaxRPMFraction =1.0;
+                ShooterConstantsBase.gearing=1;
+                ShooterConstantsBase.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER;
+                ShooterConstantsBase.resetMode = DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+                ShooterConstantsBase.frontMotorDirection = DcMotorSimple.Direction.REVERSE;
+                ShooterConstantsBase.rearMotorDirection = DcMotorSimple.Direction.REVERSE;
+                ShooterConstantsBase.startPosition =0;
+                ShooterConstantsBase.targetPosition =0;
+                ShooterConstantsBase.tolerableError=0.7; //in degrees
+                ShooterConstantsBase.FRONT_Kp=0.3;  //to be tuned for the PID controller
+                ShooterConstantsBase.FRONT_Ki=0;
+                ShooterConstantsBase.FRONT_Kd=0;
+                ShooterConstantsBase.FRONT_feedforward=0.3;
+                ShooterConstantsBase.REAR_Kp=0.3;
+                ShooterConstantsBase.REAR_Ki=0;
+                ShooterConstantsBase.REAR_Kd=0;
+                ShooterConstantsBase.REAR_feedforward=0.3;
+
 
         }
 }
