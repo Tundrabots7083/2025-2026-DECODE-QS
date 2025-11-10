b1b6445cfd3d75199585298abfc26e88ba7b6f43
johnserb <john_serb@yahoo.com>
2025-11-10 15:07:53 -0500

    added carousel place holder code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwareConfig/actuators/carousel/CarouselMotionProfilerConstants.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.hardwareConfig.actuators.carousel;
+
+import org.firstinspires.ftc.teamcode.hardwareConfig.baseConstants.MotionProfilerConstants;
+
+public class CarouselMotionProfilerConstants {
+    static {
+        MotionProfilerConstants.maxVelocity=30;  //deg/s to be tuned for the motion profiler
+        MotionProfilerConstants.maxAcceleration=10;  // deg/s**2
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwareConfig/actuators/carousel/CarouselPIDFControllerConstants.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.hardwareConfig.actuators.carousel;
+
+import org.firstinspires.ftc.teamcode.hardwareConfig.baseConstants.PIDFControllerConstants;
+
+public class CarouselPIDFControllerConstants {
+    static {
+        PIDFControllerConstants.kp=0.01;  //to be tuned for the PID controller
+        PIDFControllerConstants.ki=0.0001;
+        PIDFControllerConstants.kd=0.0;
+        PIDFControllerConstants.kf=0.0;
+        PIDFControllerConstants.maxIntegralSum=100;
+        PIDFControllerConstants.motorMinPowerLimit=-1;
+        PIDFControllerConstants.motorMaxPowerLimit=1;
+    }
+}
