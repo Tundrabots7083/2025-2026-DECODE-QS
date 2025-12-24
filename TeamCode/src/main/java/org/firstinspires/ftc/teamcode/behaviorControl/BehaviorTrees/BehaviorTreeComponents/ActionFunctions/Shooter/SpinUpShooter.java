e3310b3d2465582567619ee5d17d5f15b9bc9d36
jonathanserb <jonathanserb@gmail.com>
2025-12-23 19:19:44 -0500

    Preliminary SpinUpShooter action

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/behaviorControl/BehaviorTrees/BehaviorTreeComponents/ActionFunctions/Shooter/SpinUpShooter.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.behaviorControl.BehaviorTrees.BehaviorTreeComponents.ActionFunctions.Shooter;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.behaviorControl.BehaviorTrees.BehaviorTreeComponents.general.ActionFunction;
+import org.firstinspires.ftc.teamcode.behaviorControl.BehaviorTrees.BehaviorTreeComponents.general.BlackBoard;
+import org.firstinspires.ftc.teamcode.behaviorControl.BehaviorTrees.BehaviorTreeComponents.general.Status;
+import org.firstinspires.ftc.teamcode.hardwareControl.actuators.shooter.ShooterController;
+
+public class SpinUpShooter implements ActionFunction {
+
+    Telemetry telemetry;
+    ShooterController shooterController;
+    Status lastStatus = Status.FAILURE;
+    Status status;
+    private double targetVelocity;
+
+    public SpinUpShooter(Telemetry telemetry, ShooterController shooterController) {
+        this.telemetry = telemetry;
+        this.shooterController = shooterController;
+    }
+
+    public Status perform(BlackBoard blackBoard) {
+        if (lastStatus == Status.SUCCESS) {
+            return lastStatus;
+        }
+
+        targetVelocity = (double) blackBoard.getValue("shooterTargetVelocity");
+
+        // Activate the shooter mechanism
+        shooterController.spinToTargetVelocity(targetVelocity);
+
+        if (!shooterController.isOnTarget()) {
+            status = Status.RUNNING;
+        } else {
+            status = Status.SUCCESS;
+        }
+
+        lastStatus = status;
+        return status;
+    }
+}
